4d40f2c86d47753e95d4b05fdef73e1f04636710
Arti199424 <115499333+Arti199424@users.noreply.github.com>
2023-08-07 06:28:18 -0700

    Add files via upload

---
+++ Ep-05_Part-01/constants.js
@@ -0,0 +1,8 @@
+ export const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const CDN_LOGO="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs-2DgUY4q0-8V4K5LHgNWNgE5ujwRZvtp2A&usqp=CAU";
+
+// export default CDN_LOGO;
+// export default CDN_URL; WE CAN NOT WRITW DEFAULT TWICE..
+//a module cannot have multiple default exports....so remove this issue we should use a "Naming Exports... to export multiple things in one file"...so can write export infront your variable or const..
+
